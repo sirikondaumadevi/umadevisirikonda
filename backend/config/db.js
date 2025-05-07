cc14fb2feda9b2894a0c57be719ca7396a86950c
Sirikonda Umadevi <umadevi.sirikonda15@gmail.com>
2025-05-07 20:43:25 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,6 +9,8 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
+console.log("User:", process.env.DB_USER);
+console.log("Password:", process.env.DB_PASSWORD);
 
 connection.connect((err) => {
   if (err) {
